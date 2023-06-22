d189dbbd2e5ef5a14daa2433e53994bb485d7ce1
FernandoCalico <93493964+FernandoCalico@users.noreply.github.com>
2023-06-22 12:30:27 -0300

    Add files via upload

---
+++ exercicio_tsc.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplica = (valor1, valor2) => valor1 * valor2;
+const dizOi = (nome) => 'Olá ' + nome;
+console.log(multiplica(10, 20));
+console.log(dizOi('Fernando'));
---
+++ exercicio_tsc.ts
@@ -0,0 +1,5 @@
+const multiplica = (valor1:number, valor2:number):number => valor1 * valor2;
+const dizOi = (nome:string):string => 'Olá ' + nome;
+
+console.log(multiplica(10,20));
+console.log(dizOi('Fernando'));
